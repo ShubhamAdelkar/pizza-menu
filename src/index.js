a30dceedb4d73332845a19204ee7a4c8fa0dae4b
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-27 22:28:05 +0530

    add conditional css for sold out pizzas

--- src/index.js
+++ src/index.js
@@ -90,15 +90,15 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{"$" + pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : "$" + pizzaObj.price}</span>
       </div>
     </li>
   );
